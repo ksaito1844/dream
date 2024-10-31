1ab85abfeffb214b9d0272a496a763a9a0fcb164
ksaito1844 <ksaito1844@gmail.com>
2024-10-31 09:43:41 -0400

    Create page.js

---
+++ app/chat/page.js
@@ -0,0 +1,12 @@
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import VoiceChat from "@/Components/VoiceChat";
+
+export default function Home() {
+  return (
+    <div>
+      <VoiceChat />
+    </div>
+  );
+}
