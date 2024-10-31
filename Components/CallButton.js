8afe0a65e453c1cb808e0f57888bd975e65fab9b
ksaito1844 <ksaito1844@gmail.com>
2024-10-31 08:45:56 -0400

    Update CallButton.js

--- Components/CallButton.js
+++ Components/CallButton.js
@@ -1,1 +1,14 @@
+'use client'
+import React, { useContext } from 'react';
+import { AgoraContext } from '@/context/voiceContext';
 
+const CallButton = ({ onClick }) => {
+    const { isJoined } = useContext(AgoraContext);
+    return (
+        <button onClick={onClick}>
+            {isJoined ? 'Leave' : 'Start Audio Chat'}
+        </button>
+    );
+};
+
+export default CallButton;
